66501c6370517324139cc0d0151e27e5d98a0486
auraecosystem <sandraisom@icloud.com>
2026-06-02 23:30:08 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,11 @@
 pip install maturin
 cd python
 maturin develop
+cd simulations
+
+# Attack simulations
+python attack_simulations.py               # Core attack simulation framework
+python attack_track_fb.py                  # Trust manipulation attacks
+python attack_track_fc.py                  # Economic attacks
+
+# For full 4-Life game demos, see: https://github.com/dp-web4/4-life
